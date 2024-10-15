0aa28f1120b1725c6f361334529c173e10b1cd83
Aartell02 <artur.kuzel@gmail.com>
2024-10-15 15:26:17 +0200

    spectre console i program

--- Metronome/Program.cs
+++ Metronome/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace Metronome
+﻿using Spectre.Console;
+namespace Metronome
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("branh");
+            
         }
     }
 }
---
+++ Metronome/SCInterface.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Spectre.Console;
+
+namespace Metronome
+{
+    internal class SCInterface
+    {
+        static void Interface()
+        {
+            var table = new Table();
+            table.AddColumn(new TableColumn("Screen").Centered());
+            table.AddRow("left");
+            table.Expand();
+            AnsiConsole.Write(table);
+        }
+    }
+}
